54e36d5b4886c621bcf549ad986c94619e505e52
AWGcoding <anton.wahlstrom.gren@student.forsbergsskola.se>
2020-11-12 11:39:00 +0100

    Add script to Currency text

---
+++ Assets/SoulCount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoulCount : MonoBehaviour
+{
+    public TMPro.TMP_Text soulText;
+    public double souls;
+
+    public void Start()
+    {
+        souls = 1;
+    }
+
+    public void Update()
+    {
+        soulText.text = "Souls:  " + souls;
+    }
+}
