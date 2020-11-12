37d416659eae0aaa29a87c552cec9d8b486e4c7d
Thomas Ahlström <thomas.ahlstrom@student.forsbergsskola.se>
2020-11-12 11:27:54 +0100

    Adds create undead button

--- Assets/Scripts/Undead.cs
+++ Assets/Scripts/Undead.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
-public class Undead : MonoBehaviour
-{
+public class Undead : MonoBehaviour {
     //Placeholder
     public int currentCurrency = 250;
 
@@ -17,22 +17,27 @@ public class Undead : MonoBehaviour
 
     public TextMeshProUGUI TMP_costsText;
 
-    public bool IsAffordable => currentCurrency >= this.cost; 
-    
-    public void DisplayTexts()
-    {
+    public bool IsAffordable => currentCurrency >= this.cost;
+
+    public void DisplayTexts() {
         this.TMP_costsText.text = $"Add {name} for {cost} souls";
     }
-    
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+    }
+
+    public Button createUndead;
+    public void CreateUndead() {
+        count += 1;
+        Debug.Log("Count:"+count);
+    }
+
+    public void CreateUndeadButton() {
+        CreateUndead();
     }
 }
