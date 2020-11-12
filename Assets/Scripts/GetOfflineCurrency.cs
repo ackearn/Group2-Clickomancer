1065cc4e2884aa5b90bc08330c3b6aff8c385075
Linus Åkesson <linus.akesson@student.forsbergsskola.se>
2020-11-12 09:31:28 +0100

    Update GetOfflineCurrency.cs
    Added placeholderSouls variable + property,
    
    Property Saves and Loads PlayerPref  "SoulsEarned"
    
    Added barebones Awake and OnApplicationQuit methods

--- Assets/Scripts/GetOfflineCurrency.cs
+++ Assets/Scripts/GetOfflineCurrency.cs
@@ -1,17 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GetOfflineCurrency : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    //Placeholders, replace with currency-hook-in-references later
+    public int placeholderSouls;
+
+    public int PlaceholderSouls
+    {
+        get => PlayerPrefs.GetInt("SoulsEarned", 0);
+        private set => PlayerPrefs.SetInt("SoulsEarned", value);
+    }
+    
+    
+    private void Awake()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnApplicationQuit()
     {
         
     }
