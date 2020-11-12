feaef3eab7f1f3d4a937396100323e4b710fb138
Linus Åkesson <linus.akesson@student.forsbergsskola.se>
2020-11-12 09:54:07 +0100

    Update GetOfflineCurrency.cs
    Change PlaceholderCurrency to OfflineTime property
    
    Adds Save function to save DateTime.Now
    
    Adds DateTime.Now in Awake as local variable for comparison

--- Assets/Scripts/GetOfflineCurrency.cs
+++ Assets/Scripts/GetOfflineCurrency.cs
@@ -7,22 +7,24 @@ public class GetOfflineCurrency : MonoBehaviour
 {
     
     //Placeholders, replace with currency-hook-in-references later
-    public int placeholderSouls;
+    //public int placeholderSouls;
 
-    public int PlaceholderSouls
+    public DateTime OfflineTime
     {
-        get => PlayerPrefs.GetInt("SoulsEarned", 0);
-        private set => PlayerPrefs.SetInt("SoulsEarned", value);
+        get => Convert.ToDateTime(PlayerPrefs.GetString("SoulsEarnedOffline", "0"));
+        private set => PlayerPrefs.SetString("SoulsEarnedOffline", value.ToString());
     }
     
     
     private void Awake()
     {
+        var currentTime = DateTime.Now;
         
+
     }
 
     private void OnApplicationQuit()
     {
-        
+        OfflineTime = DateTime.Now;
     }
 }
