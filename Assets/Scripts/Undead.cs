f738a2b4bebff78b415305bc2587eed1a031c3d7
Linus Åkesson <linus.akesson@student.forsbergsskola.se>
2020-11-12 11:09:14 +0100

    Adds configurable fields and DisplayText functionality to Undead.cs

--- Assets/Scripts/Undead.cs
+++ Assets/Scripts/Undead.cs
@@ -1,9 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Undead : MonoBehaviour
 {
+    //Placeholder
+    public int currentCurrency = 250;
+
+    [SerializeField] private string name;
+    [SerializeField] private int cost;
+    [SerializeField] private int productionRate;
+    [SerializeField] private int count;
+    [SerializeField] private int level;
+    [SerializeField] private Sprite sprite;
+
+    public TextMeshProUGUI TMP_costsText;
+
+    public bool IsAffordable => currentCurrency >= this.cost; 
+    
+    public void DisplayTexts()
+    {
+        this.TMP_costsText.text = $"Add {name} for {cost} souls";
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
