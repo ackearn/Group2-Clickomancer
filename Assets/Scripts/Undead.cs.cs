f9209cff957b779fe63c1d74fa1f658dd8269509
Linus Åkesson <linus.akesson@student.forsbergsskola.se>
2020-11-12 10:56:04 +0100

    Update Undead.cs.cs
    fix name

--- Assets/Scripts/Undead.cs.cs
+++ Assets/Scripts/Undead.cs.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Undead.cs : MonoBehaviour
+public class Undead : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
